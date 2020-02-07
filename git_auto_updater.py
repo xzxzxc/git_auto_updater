e16e785e9e8999b6c191c089d1242bcf189d711b
Andrii Rybalko <rybalko.andriy@gmail.com>
2020-02-07 12:54:58 +0200

    small refctoring

--- git_auto_updater.py
+++ git_auto_updater.py
@@ -2,20 +2,21 @@ from subprocess import Popen, check_output
 from threading import Timer
 from os import path
 
-def get_current_commit(soft_path: str, git_repo_url: str) -> str:
-	output = check_output('git rev-parse HEAD'.split(), shell=True, cwd=soft_path).decode('utf-8')
-	return output.strip()
+def get_local_commit(soft_path: str, git_repo_url: str) -> str:
+	return check_output('git rev-parse HEAD'.split(), shell=True, cwd=soft_path).decode('utf-8').strip()
 
-def get_commit_from_git(git_repo_url: str, prod_branch: str) -> str:
+def get_remote_commit(git_repo_url: str, prod_branch: str) -> str:
 	output = check_output(f'git ls-remote {git_repo_url}'.split(), shell=True).decode('utf-8')
 	for l in output.split('\n'):
-		commit, branch = l.split('\t')[:2]
+		parts = l.split('\t')
+		if len(parts) < 2:
+			continue
+		commit, branch = parts[:2]
 		if branch == 'refs/heads/' + prod_branch:
 			return commit
-	raise Exception(f'could not find {prod_branch} branch in {git_repo_url}')
+	raise Exception(f'could not find repo {git_repo_url} or branch {prod_branch}')
 
-flatten = lambda l: [item for sublist in l for item in sublist]
-def pull_last_version(soft_path: str, git_repo_url: str, prod_branch: str) -> None:
+def pull_latest_version(soft_path: str, git_repo_url: str, prod_branch: str) -> None:
 	commands = [f'git checkout {prod_branch}', 'git pull'] \
 		if path.isdir(path.join(soft_path, '.git')) \
 		else [f'git clone -b {prod_branch} {git_repo_url} {soft_path}']
@@ -23,16 +24,14 @@ def pull_last_version(soft_path: str, git_repo_url: str, prod_branch: str) -> No
 		out = check_output(cmd.split(' '), shell=True, cwd=soft_path)
 
 def try_update(soft_path: str, startup_command: str, git_repo_url: str, prod_branch: str) -> bool:
-	cur_ver = get_current_commit(soft_path, git_repo_url)
-	last_ver = get_commit_from_git(git_repo_url, prod_branch)
+	cur_ver = get_local_commit(soft_path, git_repo_url)
+	last_ver = get_remote_commit(git_repo_url, prod_branch)
 	res = cur_ver != last_ver
 	if res:
-		pull_last_version(soft_path, git_repo_url, prod_branch)
+		pull_latest_version(soft_path, git_repo_url, prod_branch)
 	return res
 
-pipe: Popen = None
-def process(soft_path: str, startup_command: str, git_repo_url: str, prod_branch: str, allow_shut_down: bool, interval: int, kwargs) -> None:
-	global pipe
+def process(soft_path: str, startup_command: str, git_repo_url: str, prod_branch: str, allow_shut_down: bool, interval: int, pipe: Popen , kwargs) -> None:
 	Timer(args.mins * 60, process, kwargs=kwargs).start()
 	updated = try_update(soft_path, startup_command, git_repo_url, prod_branch)
 	if updated and allow_shut_down and pipe is not None:
@@ -49,16 +48,18 @@ if __name__ == '__main__':
 	parser.add_argument('-g', '--git', help='url to git repository', required=True)
 	parser.add_argument('-b', '--branch', default='master', help='branch from which software would be taken')
 	parser.add_argument('-m', '--mins', default=60, type=int, help='interval in minutes to check for update')
-	parser.add_argument('-dsd', '--disallowShutDown', action='store_true', help='disallow to shutdown software if new version found')
+	parser.add_argument('-dsd', '--disallow-shut-down', action='store_true', help='disallow to shutdown software if new version found')
 	args = parser.parse_args()
+	pipe: Popen = None
 	kwargs = {
 		'soft_path': args.path,
 		'startup_command': args.cmd,
 		'git_repo_url': args.git,
 		'prod_branch': args.branch,
-		'allow_shut_down': not args.disallowShutDown,
+		'allow_shut_down': not args.disallow_shut_down,
 		'interval': args.mins,
+		'pipe': pipe,
 	}
 	kwargs['kwargs'] = kwargs # kwargs is kek:)
-	process(**kwargs) # first call
+	process(**kwargs) # start processing
 	input('Process starting, press any key to shut down')
